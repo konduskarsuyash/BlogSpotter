732cda98d5ff9f95452a8c914b2a159274987e6f
konduskarsuyash <suyashkonduskar27@gmail.com>
2024-03-26 01:25:49 +0530

    User account and profilr picture 7th

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -4,11 +4,10 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
 app = Flask(__name__)
-app.debug = True
-app.config['SECRET_KEY'] = 'secret'
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
-bcrypt  = Bcrypt(app)
+bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
