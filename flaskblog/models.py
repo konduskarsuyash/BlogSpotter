723ab472befe15aabb3bd8b0a2c2567d6081ffdf
konduskarsuyash <suyashkonduskar27@gmail.com>
2024-03-28 19:27:21 +0530

    reset password 11th

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,18 +1,33 @@
-from flaskblog import db,login_manager
 from datetime import datetime
+from itsdangerous import URLSafeTimedSerializer
+from flask import current_app
 from flask_login import UserMixin
+from flaskblog import db, login_manager
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-class User(db.Model,UserMixin):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
+
+    def get_reset_token(self, expires_sec=1800):
+        s = URLSafeTimedSerializer(current_app.config['SECRET_KEY'])
+        return s.dumps({'user_id': self.id})
+
+    @staticmethod
+    def verify_reset_token(token):
+        s = URLSafeTimedSerializer(current_app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return User.query.get(user_id)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
